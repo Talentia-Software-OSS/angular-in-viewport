f80874c8af480298254e046e4193cd3628091e7f
Leonardo Cavone <lcavone@talentia-software.com>
2025-10-07 11:44:14 +0200

    fixed eslint rules

--- projects/angular-in-viewport/eslint.config.js
+++ projects/angular-in-viewport/eslint.config.js
@@ -7,8 +7,11 @@ module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
     rules: {
+      "@angular-eslint/no-empty-lifecycle-method": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-explicit-any": "off",
       "@angular-eslint/directive-selector": [
-        "error",
+        "off",
         {
           type: "attribute",
           prefix: "lib",
